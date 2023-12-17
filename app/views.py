e89ba0db9ca925302049c7742e923b28e899ed17
vamshi0568 <vamshimukkala939@gmail.com>
2023-12-17 22:55:43 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def srujana(request):
+    return render(request,'srujana.html')
+
+def sudheer(request):
+    return render(request,'sudheer.html')
